17cc6af470010cf090c33e7ffcc8c646efa30735
Chris Knight <chris.knight@ticketmaster.co.uk>
2017-06-22 08:54:28 +0100

    Environment settings, just need to get the rooting right so it can see it

--- trunk/src/app/angular2/app/main.ts
+++ trunk/src/app/angular2/app/main.ts
@@ -1,5 +1,11 @@
 // main entry point
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { enableProdMode } from '@angular/core';
+//import { environment } from '../environments/environment';
 import { AppModule } from './app.module';
+
+// if (environment.production) {
+//   enableProdMode();
+// }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
